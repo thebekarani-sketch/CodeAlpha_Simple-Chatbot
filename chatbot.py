dd054e34e05e1c1e7135fa43dbe460ab547726de
Thebekarani <thebekarani@gmail.com>
2026-06-25 14:22:56 +0530

    Adding greetings

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,12 @@
 def chatbot():
     while True:
-        user_input = input("You: ")
-        print("Bot: You said", user_input)
+        user_input = input("You: ").lower()
+        if user_input == "hello":
+            print("Bot: Hi!")
+        elif user_input == "bye":
+            print("Bot: Goodbye!")
+            break
+        else:
+            print("Bot: I'm not sure how to respond to that.")
 
 chatbot()
